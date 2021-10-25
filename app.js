ec628dcdebe67e5d9c63b0dbe9bbcbd3407a61f3
jonathancklee1 <85152302+jonathancklee1@users.noreply.github.com>
2021-10-25 18:31:51 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
-  const btn = question.querySelector(".question-btn");
-  // const questionTitle = question.querySelector(".question-text");
+  const btn = question.querySelector(".question-title");
+
   btn.addEventListener("click", function () {
     questions.forEach(function (item) {
       if (item !== question) {
         item.classList.remove("show-text");
-        // item.classList.remove("flip");
-        // questionTitle.classList.remove("bold");
       }
     });
     console.log(btn);
     question.classList.toggle("show-text");
-    // btn.classList.toggle("flip");
-    // questionTitle.classList.toggle("bold");
   });
 });
